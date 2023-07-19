dd81fa142aae4c763e5c62fc3f56f1bb28f1ec90
Kody Kregulka <kreg@Madalyns-MacBook-Air.local>
2023-07-19 13:32:47 -0400

    added health and wasd controls

--- turn-based-dungeoncrawler/src/systems/player_input.rs
+++ turn-based-dungeoncrawler/src/systems/player_input.rs
@@ -3,6 +3,8 @@ use crate::prelude::*;
 #[system]
 #[read_component(Point)]
 #[read_component(Player)]
+#[read_component(Enemy)]
+#[write_component(Health)]
 pub fn player_input(
 	ecs: &mut SubWorld,
 	commands: &mut CommandBuffer,
@@ -12,9 +14,13 @@ pub fn player_input(
 	if let Some(key) = key {
 		let delta = match key {
 			VirtualKeyCode::Left => Point::new(-1, 0),
+			VirtualKeyCode::A => Point::new(-1, 0),
 			VirtualKeyCode::Right => Point::new(1, 0),
+			VirtualKeyCode::D => Point::new(1, 0),
 			VirtualKeyCode::Up => Point::new(0, -1),
+			VirtualKeyCode::W => Point::new(0, -1),
 			VirtualKeyCode::Down => Point::new(0, 1),
+			VirtualKeyCode::S => Point::new(0, 1),
 			_ => Point::new(0, 0),
 		};
 
@@ -25,6 +31,7 @@ pub fn player_input(
 			.unwrap();
 
 		let mut enemies = <(Entity, &Point)>::query().filter(component::<Enemy>());
+		let mut did_something = false;
 		if delta.x != 0 || delta.y != 0 {
 			let mut hit_something = false;
 			enemies
@@ -32,6 +39,7 @@ pub fn player_input(
 				.filter(|(_, pos)| **pos == destination)
 				.for_each(|(entity, _)| {
 					hit_something = true;
+					did_something = true;
 
 					commands.push((
 						(),
@@ -43,6 +51,7 @@ pub fn player_input(
 				});
 
 			if !hit_something {
+				did_something = true;
 				commands.push((
 					(),
 					WantsToMove {
@@ -50,6 +59,15 @@ pub fn player_input(
 						destination,
 					},
 				));
+			}
+		}
+		if !did_something {
+			if let Ok(mut health) = ecs
+				.entry_mut(player_entity)
+				.unwrap()
+				.get_component_mut::<Health>()
+			{
+				health.current = i32::min(health.max, health.current + 1);
 			}
 		}
 		*turn_state = TurnState::PlayerTurn;
