4e9d7f2442cb19a0894c6dd1a5f8e306597fbb05
kodykregulka <kodykregulka@gmail.com>
2023-07-19 01:06:04 -0400

    implements movement with command buffer

--- turn-based-dungeoncrawler/src/components.rs
+++ turn-based-dungeoncrawler/src/components.rs
@@ -6,6 +6,12 @@ pub struct Render {
 	pub glyph: FontCharType,
 }
 
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct WantsToMove {
+	pub entity: Entity,
+	pub destination: Point,
+}
+
 //tag components
 //empty struct containing no data indicating that an entity with this component is the player
 #[derive(Clone, Copy, Debug, PartialEq)]
