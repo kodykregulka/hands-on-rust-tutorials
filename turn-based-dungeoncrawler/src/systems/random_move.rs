b8c460db6655641bc5f2f63412bb27fc17bba695
Kody Kregulka <kreg@Madalyns-MacBook-Air.local>
2023-07-18 23:35:16 -0400

    random movement

--- turn-based-dungeoncrawler/src/systems/random_move.rs
+++ turn-based-dungeoncrawler/src/systems/random_move.rs
@@ -6,6 +6,13 @@ pub fn random_move(ecs: &mut SubWorld, #[resource] map: &Map){
 	movers.
 		.iter_mut(ecs)
 		.for_each(|(pos, _)| {
-			
+			let mut rng = RanddomNumberGenerator::new();
+			let destination = match rng.range(0,4){
+				0 => Point::new(-1,0),
+				1=> Point::new(1,0),
+				2=> Point::new(0,-1),
+				3=> Point::new(0,1),
+			} + *pos;
+
 		})
 }
