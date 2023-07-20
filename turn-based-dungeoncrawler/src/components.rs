55aca0ed6e7335ea7a6a7e2d19173478a7fc252d
Kody Kregulka <kreg@Madalyns-MacBook-Air.local>
2023-07-20 00:45:38 -0400

    implemented victory

--- turn-based-dungeoncrawler/src/components.rs
+++ turn-based-dungeoncrawler/src/components.rs
@@ -37,3 +37,7 @@ pub struct Enemy;
 pub struct MovingRandomly;
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct ChasingPlayer;
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Item;
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct AmuletOfYala;
