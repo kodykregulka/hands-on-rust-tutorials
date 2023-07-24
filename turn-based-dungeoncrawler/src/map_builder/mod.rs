d9383a81baf0c2abca8c2ea5b80adea52c1b789b
kodykregulka <kodykregulka@gmail.com>
2023-07-23 23:46:51 -0400

    implements drunk, automata, and prefab maps

--- turn-based-dungeoncrawler/src/map_builder/mod.rs
+++ turn-based-dungeoncrawler/src/map_builder/mod.rs
@@ -1,9 +1,15 @@
-use crate::prelude::*;
-use empty::EmptyArchitect;
-use rooms::RoomsArchitect;
-
+mod automata;
+mod drunkard;
 mod empty;
+mod prefab;
 mod rooms;
+
+use crate::prelude::*;
+use automata::CellularAutomataArchitect;
+use drunkard::DrunkardsWalkArchitect;
+use empty::EmptyArchitect;
+use prefab::apply_prefab;
+use rooms::RoomsArchitect;
 
 trait MapArchitect {
 	fn new(&mut self, rng: &mut RandomNumberGenerator) -> MapBuilder;
@@ -21,8 +27,15 @@ pub struct MapBuilder {
 
 impl MapBuilder {
 	pub fn new(rng: &mut RandomNumberGenerator) -> Self {
-		let mut architect = RoomsArchitect {};
-		architect.new(rng)
+		let mut architect: Box<dyn MapArchitect> = match rng.range(0, 3) {
+			0 => Box::new(DrunkardsWalkArchitect {}),
+			1 => Box::new(RoomsArchitect {}),
+			_ => Box::new(CellularAutomataArchitect {}),
+		};
+
+		let mut mb = architect.new(rng);
+		apply_prefab(&mut mb, rng);
+		mb
 	}
 
 	fn fill(&mut self, tile: TileType) {
@@ -112,5 +125,29 @@ impl MapBuilder {
 				self.apply_horizontal_tunnel(prev.x, new.x, new.y);
 			}
 		}
+	}
+
+	fn spawn_monsters(&self, start: &Point, rng: &mut RandomNumberGenerator) -> Vec<Point> {
+		const NUM_MONSTERS: usize = 50;
+		let mut spawnable_tiles: Vec<Point> = self
+			.map
+			.tiles
+			.iter()
+			.enumerate()
+			.filter(|(idx, t)| {
+				**t == TileType::Floor
+					&& DistanceAlg::Pythagoras.distance2d(*start, self.map.index_to_point2d(*idx))
+						> 10.0
+			})
+			.map(|(idx, _)| self.map.index_to_point2d(idx))
+			.collect();
+
+		let mut spawns = Vec::new();
+		for _ in 0..NUM_MONSTERS {
+			let target_index = rng.random_slice_index(&spawnable_tiles).unwrap();
+			spawns.push(spawnable_tiles[target_index].clone());
+			spawnable_tiles.remove(target_index);
+		}
+		spawns
 	}
 }
