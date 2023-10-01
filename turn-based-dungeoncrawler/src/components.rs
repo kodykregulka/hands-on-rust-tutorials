6fd6bda583fc06179c0ac08e97dc329afc15db44
kodykregulka <kodykregulka@gmail.com>
2023-10-01 11:12:43 -0400

    adds health potions and map revealing items

--- turn-based-dungeoncrawler/src/components.rs
+++ turn-based-dungeoncrawler/src/components.rs
@@ -41,8 +41,25 @@ pub struct Health {
 	pub max: i32,
 }
 
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct ProvidesHealing{
+	pub amount: i32
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct ProvidesDungeonMap;
+
 #[derive(Clone, PartialEq)]
 pub struct Name(pub String);
+
+#[derive(Clone, PartialEq)]
+pub struct Carried(pub Entity);
+
+#[derive(Clone, PartialEq)]
+pub struct ActivateItem {
+	pub used_by : Entity,
+	pub item : Entity
+}
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct FieldOfView {
